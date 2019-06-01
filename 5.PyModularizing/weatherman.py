4fe5773b44f6d32bdd0078b2dba4971d96e9a155
cinexin <migueguillamon@gmail.com>
2019-06-01 19:27:53 +0200

    modules and packages

--- 5.PyModularizing/weatherman.py
+++ 5.PyModularizing/weatherman.py
@@ -4,8 +4,7 @@ from sources import daily, weekly
 description = wr.get_description()
 print("todays weather:",description)
 
-print ("Daily forecast:", daily.forecast())
-print ("Weekly forecast:")
-for number, outlook in enumerate(weekly.forecast(),1):
+print("Daily forecast:", daily.forecast())
+print("Weekly forecast:")
+for number, outlook in enumerate(weekly.forecast(), 1):
     print(number, outlook)
-    
