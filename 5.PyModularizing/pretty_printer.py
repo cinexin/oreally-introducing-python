7759d0c7e8ff9dc1f2805e106a0b2c6663e59e21
cinexin <migueguillamon@gmail.com>
2019-06-08 10:39:34 +0200

    itertools and pretty printer

---
+++ 5.PyModularizing/pretty_printer.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+from collections import OrderedDict
+
+quotes = OrderedDict([
+    ('Moe', 'A wise guy, huh?'),
+    ('Larry', 'Ow!'),
+    ('Curly', 'Nyuk nyuk!')
+])
+
+print(quotes)
+pprint(quotes)
--- 5.PyModularizing/stdlib_useful_collections/itertools.py
+++ 5.PyModularizing/stdlib_useful_collections/itertools.py
@@ -5,3 +5,17 @@ Each returns one item at a time when called within a for loop, and remembers its
 
 for item in itertools.chain([1, 2], ['a', 'b']):
     print(item)
+    
+for item in itertools.cycle([1,2]):
+    print(item)
+    
+import itertools
+for item in itertools.accumulate([1,2,3,4]):
+    print(item)
+    
+import itertools
+def multiply(a,b):
+    return a*b
+
+for item in itertools.accumulate([1,2,3,4], multiply):
+    print(item)
