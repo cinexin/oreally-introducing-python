601e47c7b5d114aedc083af81a3bde2232b74371
cinexin <migueguillamon@gmail.com>
2019-08-24 12:28:03 +0200

    ZeroMQ basic TPC IP messaging

---
+++ 11.ConcurrencyAndNetworks/ZeroMQ/Sockets/MessagesServer.py
@@ -0,0 +1,16 @@
+import zmq
+
+host = '127.0.0.1'
+port = 6789
+context = zmq.Context()
+server = context.socket(zmq.REP)
+server.bind('tcp://%s:%s' %(host, port))
+while True:
+    print('Waiting for someone to tell me something')
+    # Wait for next req from client
+    request_bytes = server.recv()
+    request_str = request_bytes.decode('utf-8')
+    print ("That voice in my head says: %s" %request_str)
+    reply_str = "Stop saying %s" % request_str
+    reply_bytes = bytes(reply_str,'utf-8')
+    server.send(reply_bytes)
