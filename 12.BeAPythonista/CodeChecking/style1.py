5ac01c3427dd59623e1aef92b172d51ec616e647
cinexin <migueguillamon@gmail.com>
2019-08-26 19:04:58 +0200

    Check code style examples

---
+++ 12.BeAPythonista/CodeChecking/style1.py
@@ -0,0 +1,16 @@
+'''
+We can run pre-checker Python code.
+Most popular are:
+pylint
+pyflakes
+
+pip install pylint
+pip install pyflakes
+pip install pycodestyle
+'''
+A = 1
+B = 2
+C = 3
+print(A)
+print(B)
+print(C)
